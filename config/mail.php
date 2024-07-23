cb8229ce5a5fdb2cee40b8464c1d1292e323cf1a
russell <russell@russells-MacBook-Pro.local>
2024-07-23 11:52:45 +0100

    added logging to test on postfix

--- config/mail.php
+++ config/mail.php
@@ -41,11 +41,15 @@ return [
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
-            'encryption' => env('MAIL_ENCRYPTION', null),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
+
+
+            'auth_mode'  => null,
+            'verify_peer'       => false,
         ],
 
         'ses' => [
